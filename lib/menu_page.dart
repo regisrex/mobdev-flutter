c6e6082527db47774f50ce6b41f39d7b95e9e8c6
ndzhwr <ndizihiweregis06@gmail.com>
2023-02-20 17:11:17 +0200

    stuff

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontendmasters/components/productitem.dart';
 import 'package:flutter_frontendmasters/store/datamodel.dart';
 import 'package:flutter_frontendmasters/utils/datamanager.dart';
 
@@ -20,31 +21,51 @@ class MenuPage extends StatelessWidget {
       future: datamanager.getMenu(),
       builder: ((context, snapshot) {
         if (snapshot.hasData) {
-          return const Center(child: Text("Has data"));
-        } else {
-          if (snapshot.hasData) {
-            return Center(
-              child: SizedBox(
-                height: 100,
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.cloud_off,
+          List<Category> categories = snapshot.data!;
+          return ListView.builder(
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(categories[index].name),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: categories[index].products.length,
+                    itemBuilder: (ccontext, iindex) {
+                      return ProductItem(
+                          product: categories[index].products[iindex],
+                          onAdd: dispatchWhenAdd(
+                              categories[index].products[iindex]));
+                    },
+                  )
+                ],
+              );
+            },
+          );
+        } else if (!snapshot.hasData) {
+          return Center(
+            child: SizedBox(
+              height: 100,
+              child: Column(
+                children: const [
+                  Icon(
+                    Icons.cloud_off,
+                    color: Colors.grey,
+                    size: 40,
+                  ),
+                  Text(
+                    "Nothing on our menu!",
+                    style: TextStyle(
                       color: Colors.grey,
-                      size: 40,
+                      fontSize: 14,
                     ),
-                    Text(
-                      "Nothing on our menu!",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            );
-          }
+            ),
+          );
         }
         return const Center(child: CircularProgressIndicator());
       }),
