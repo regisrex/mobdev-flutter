79f3182474e8951e3ea90f8e53876eeb8da6d109
ndzhwr <ndizihiweregis06@gmail.com>
2023-02-21 06:25:08 +0200

    reactive cart

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -3,53 +3,59 @@ import 'package:flutter_frontendmasters/components/order_item.dart';
 import 'package:flutter_frontendmasters/store/datamodel.dart';
 import 'package:flutter_frontendmasters/utils/datamanager.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   final Datamanager datamanager;
   const CartPage({super.key, required this.datamanager});
 
   @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-      child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-          child: ListView.builder(
-            itemCount: datamanager.cart.length,
-            itemBuilder: (context, index) {
-              var item = datamanager.cart[index];
-              if (datamanager.cart.isNotEmpty) {
+    if (widget.datamanager.cart.isNotEmpty) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: ListView.builder(
+              itemCount: widget.datamanager.cart.length,
+              itemBuilder: (context, index) {
+                var item = widget.datamanager.cart[index];
                 return OrderItem(
                     item: item,
                     onRemove: (Product product) {
-                      datamanager.removeFromCart(product);
+                      setState(() {
+                        widget.datamanager.removeFromCart(product);
+                      });
                     });
-              } else if(datamanager.cart.isEmpty) {
-                return Center(
-                  child: SizedBox(
-                    height: 100,
-                    child: Column(
-                      children: const [
-                        Icon(
-                          Icons.cloud_off,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        Text(
-                          "Nothing on our menu!",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              }else{
-              return const Center(child: CircularProgressIndicator());
-              }
-            },
-          )),
-    );
+              },
+            )),
+      );
+    } else {
+      return Center(
+        child: SizedBox(
+          height: 100,
+          child: Column(
+            children: const [
+              Icon(
+                Icons.coffee_outlined,
+                color: Colors.grey,
+                size: 40,
+              ),
+              Text(
+                "Nothing in your cart!",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
--- lib/components/order_item.dart
+++ lib/components/order_item.dart
@@ -1,12 +1,17 @@
 import "package:flutter/material.dart";
 import "package:flutter_frontendmasters/store/datamodel.dart";
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ItemInCart item;
   final Function onRemove;
   const OrderItem({Key? key, required this.item, required this.onRemove})
       : super(key: key);
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,25 +25,26 @@ class OrderItem extends StatelessWidget {
                 flex: 1,
                 child: Padding(
                   padding: const EdgeInsets.only(left: 8.0),
-                  child: Text("${item.quantity}x"),
+                  child: Text("${widget.item.quantity}x"),
                 )),
             Expanded(
                 flex: 6,
                 child: Text(
-                  item.product.name,
+                  widget.item.product.name,
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 )),
             Expanded(
                 flex: 2,
                 // ignore: prefer_interpolation_to_compose_strings
                 child: Text("\$" +
-                    (item.product.price * item.quantity).toStringAsFixed(2))),
+                    (widget.item.product.price * widget.item.quantity)
+                        .toStringAsFixed(2))),
             Expanded(
                 flex: 1,
                 child: IconButton(
                     color: Theme.of(context).primaryColor,
                     onPressed: () {
-                      onRemove(item.product);
+                      widget.onRemove(widget.item.product);
                     },
                     icon: const Icon(Icons.delete)))
           ],
